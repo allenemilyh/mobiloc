8afe22986b96bfc250fddf06a0d7cdbb338c2aad
Emily Allen <allenemilyh@gmail.com>
2012-12-11 15:37:30 -0800

    initial mobiloc commit

---
+++ mobidata.rb
@@ -0,0 +1,22 @@
+# mobidata: the reference data array for Programming Ruby 1.9 book
+
+$book_data = [
+  { :chapter => 1, :subsection => 1, :page => 3, :location => 470, :topic => "The Command Prompt" },
+  { :chapter => 1, :subsection => 2, :page => 5, :location => 540, :topic => "Installing Ruby" },
+  { :chapter => 1, :subsection => 3, :page => 10, :location => 726, :topic => "Running Ruby" },
+  { :chapter => 1, :subsection => 4, :page => 12, :location => 796, :topic => "Ruby Documentation" },
+  { :chapter => 2, :subsection => 1, :page => 15, :location => 917, :topic => "Ruby is an Object-Oriented Language" },  
+  { :chapter => 2, :subsection => 2, :page => 17, :location => 971, :topic => "Some Basic Ruby" }, 
+  { :chapter => 2, :subsection => 3, :page => 20, :location => 1077, :topic => "Arrays and Hashes" }, 
+  { :chapter => 2, :subsection => 4, :page => 21, :location => 1142, :topic => "Symbols" }, 
+  { :chapter => 2, :subsection => 5, :page => 23, :location => 1200, :topic => "Control Structures" },  
+  { :chapter => 2, :subsection => 6, :page => 24, :location => 1261, :topic => "Regular Expressions" }, 
+  { :chapter => 2, :subsection => 7, :page => 25, :location => 1317, :topic => "Blocks and Iterators" },  
+  { :chapter => 2, :subsection => 8, :page => 27, :location => 1398, :topic => "Reading and 'Riting" },
+  { :chapter => 2, :subsection => 9, :page => 28, :location => 1424, :topic => "Command-Line Arguments" },
+  { :chapter => 2, :subsection => 10, :page => 28, :location => 1437, :topic => "Onward and Upward" },
+  { :chapter => 3, :subsection => 1, :page => 32, :location => 1609, :topic => "Objects and Attributes" },
+  { :chapter => 3, :subsection => 2, :page => 37, :location => 1846, :topic => "Classes Working with Other Classes" },
+  { :chapter => 3, :subsection => 3, :page => 40, :location => 1996, :topic => "Access Control" },
+  { :chapter => 3, :subsection => 4, :page => 43, :location => 2126, :topic => "Variables" },    
+  ] 
